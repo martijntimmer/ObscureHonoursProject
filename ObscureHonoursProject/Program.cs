ea84a08c4f54570bae36113db515aafe055a4b78
ToonBoon <toonvbenthem@gmail.com>
2017-05-11 12:51:27 +0200

    idk what I changed

--- ObscureHonoursProject/Program.cs
+++ ObscureHonoursProject/Program.cs
@@ -10,7 +10,7 @@ namespace ObscureHonoursProject
     {
         static void Main(string[] args)
         {
-            MartijnTest();
+            //MartijnTest();
             //ToonTest();
         }
 
